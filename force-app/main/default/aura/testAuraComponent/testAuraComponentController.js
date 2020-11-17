ccdaec0624296f193a39f5ebf9a5dd384183029e
Kishan_gupta_ <kishangupta032@gmail.com>
2020-11-17 12:19:44 +0530

    added some changes to testAuraComponent

--- force-app/main/default/aura/testAuraComponent/testAuraComponentController.js
+++ force-app/main/default/aura/testAuraComponent/testAuraComponentController.js
@@ -1,5 +1,6 @@
 ({
 	updateValue : function(component, event, helper) {
+        console.log('updateValue');
         var val = component.find("myInput").getElement().value;
         component.set("v.greeting", val);
     }
